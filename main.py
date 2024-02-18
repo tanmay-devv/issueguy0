70f49663412bf89225f29fdf15af2a4106f578fa
tanmay-devv <151938923+tanmay-devv@users.noreply.github.com>
2024-02-18 12:07:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from github import Github
 resultno = int(input("Enter the number of Results To Fetch:  "))
 
 # Replace 'YOUR_PERSONAL_ACCESS_TOKEN' with your actual personal access token
-g = Github("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")
+g = Github("")
 
 query = "is:issue is:open"
 issues = g.search_issues(query)
